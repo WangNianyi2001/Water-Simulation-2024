54683a90d758e2b87c78c6f2b76c1108e24cff5d
Nianyi Wang <wangnianyi2001@outlook.com>
2024-05-29 05:11:57 +0800

    Implement log outputs for the experiment

--- Assets/Scripts/Water Simulation/WaterSimulationExperimentRigidbody.cs
+++ Assets/Scripts/Water Simulation/WaterSimulationExperimentRigidbody.cs
@@ -3,12 +3,41 @@ using UnityEngine;
 namespace Nianyi.UnityPlayground.WaterSimulation {
 	public class WaterSimulationExperimentRigidbody : MonoBehaviour {
 		#region Life cycle
-		protected void OnEnterWater(Water water) {
-			Debug.Log($"{name} entered {water}.", this);
+		protected void Start() {
+			Initialize();
 		}
 
-		protected void OnExitWater(Water water) {
-			Debug.Log($"{name} exited {water}.", this);
+		protected void FixedUpdate() {
+			float t = Time.timeSinceLevelLoad;
+			if(t >= 5.0f)
+				UnityEditor.EditorApplication.isPaused = true;
+
+			var rot = transform.rotation.eulerAngles;
+			Debug.Log(string.Join(" ", new string[] {
+				$"LOGLINE", // mark
+				$"{t}", // t
+				$"{body.worldCenterOfMass.y}", // center-of-mass Y
+				$"{GetLowestY()}", // lowest Y
+				$"{rot.x}", // rotation X
+				$"{rot.y}", // rotation Y
+				$"{rot.z}", // rotation Z
+			}));
+		}
+		#endregion
+
+		#region Fields
+		private new Collider collider;
+		private Rigidbody body;
+		#endregion
+
+		#region Functions
+		private void Initialize() {
+			collider = GetComponent<Collider>();
+			body = GetComponent<Rigidbody>();
+		}
+
+		private float GetLowestY() {
+			return collider.bounds.min.y;
 		}
 		#endregion
 	}
